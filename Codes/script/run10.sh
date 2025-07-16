ba5ab3fec8f6f7b056c3cc1dedad84f34c062071
luotc-why <496679420@qq.com>
2025-07-16 15:44:49 +0800

    duoyu

--- Codes/script/run10.sh
+++ Codes/script/run10.sh
@@ -2,19 +2,19 @@
 
 python3 Codes/val_vp_detection.py \
  --mode spimg_spmask \
- --output_dir Data/output/logs\
- --device cuda:0\
- --base_dir Data/pascal-5i/ \
+ --output_dir VisualICL/output/logs\
+ --device cuda:4\
+ --base_dir VisualICL/pascal-5i/ \
  --batch-size 8 \
  --lr 0.03\
  --epoch 150\
  --arr a1\
  --vp-model Prompt\
  --p-eps 1\
- --ckpt Data/weights/checkpoint-1000.pth\
- --vq_ckpt_dir Data/weights/vqgan\
- --save_base_dir Data/\
+ --ckpt VisualICL/weights/checkpoint-1000.pth\
+ --vq_ckpt_dir VisualICL/weights/vqgan\
+ --save_base_dir VisualICL/\
  --simidx 16\
  --dropout 0.25\
  --align_q 0 \
- --save_model_path Data/ckpt/Det_K_16.pth
+ --save_model_path VisualICL/ckpt/Det_K_16.pth
